8c1b0ab3fef8a44123beb6ef87ff29421632136e
martin <martinmorondo@gmail.com>
2026-08-08 21:57:17 -0300

    fix: sincronización de mayúsculas y minúsculas en ruta de Header para Linux

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 
 import AppRoutes from "./routes";
-import Header from './components/header/Header';
+import Header from './components/header/Header.jsx';
 import AnimatedCursor from "./hooks/AnimatedCursor";
 import './App.css';
 
